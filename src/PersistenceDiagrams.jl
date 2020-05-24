9a202f6bf031dfa47c20493c2846e0699a12915d
mtsch <matijacufar@gmail.com>
2020-05-24 10:40:54 +0200

    compat

--- src/PersistenceDiagrams.jl
+++ src/PersistenceDiagrams.jl
@@ -1,9 +1,11 @@
 module PersistenceDiagrams
+
 export Infinity, ∞
 export PersistenceInterval, PersistenceDiagram
 export birth, death, persistence, representative, dim, threshold
 export Bottleneck, Wasserstein, distance, matching, Matching
 
+using Compat
 using Distances
 using Hungarian
 using RecipesBase
