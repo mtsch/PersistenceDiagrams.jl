d54257e4b97b6d29923c7af25c459c7da7eadf50
mtsch <matijacufar@gmail.com>
2020-05-24 11:05:52 +0200

    infinity tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -4,6 +4,9 @@ using Test
 @safetestset "Aqua" begin
     include("aqua.jl")
 end
+@safetestset "infinity" begin
+    include("infinity.jl")
+end
 @safetestset "diagrams" begin
     include("diagrams.jl")
 end
