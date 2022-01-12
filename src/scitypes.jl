772a43e5df1f57104ce425299d9726a99fe760da
mtsch <matijacufar@gmail.com>
2022-01-13 09:03:58 +1300

    Drop `kwargs` from `scitype` definition (#51)
    * Drop `kwargs` from `scitype` definition
    
    * bump version

--- src/scitypes.jl
+++ src/scitypes.jl
@@ -1,4 +1,4 @@
 import ScientificTypes: DefaultConvention
 const SB = ScientificTypes.ScientificTypesBase
 
-SB.scitype(::PersistenceDiagram, ::DefaultConvention; kwargs...) = PersistenceDiagram
+SB.scitype(::PersistenceDiagram, ::DefaultConvention) = PersistenceDiagram
