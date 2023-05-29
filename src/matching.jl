3972381c32932dad339b363e0f05758ca5076f5d
Ali Salehi <49132329+iamalisalehi@users.noreply.github.com>
2023-05-29 21:27:35 +0330

    Update matching.jl
    Fixed bug

--- src/matching.jl
+++ src/matching.jl
@@ -85,11 +85,11 @@ function matching(match::Matching; bottleneck=match.bottleneck)
         elseif i ≤ n
             # left is matched to diagonal
             l = match.left[i]
-            push!(result, match.left[i] => PersistenceInterval((birth(l)+death(l)), (birth(l)+death(l))))
+            push!(result, match.left[i] => PersistenceInterval((birth(l)+death(l))/2, (birth(l)+death(l))/2))
         elseif j ≤ m
             # right is matched to diagonal
             r = match.right[j]
-            push!(result, PersistenceInterval((birth(r)+death(r)), (birth(r)+death(r))) => r)
+            push!(result, PersistenceInterval((birth(r)+death(r))/2, (birth(r)+death(r))/2) => r)
         end
     end
     sort!(result)
